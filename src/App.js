ae9db6af11b9ff7acce04c261cf176759fed4ef7
B <sbslim.dev@gmail.com>
2022-01-03 11:46:23 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+
+import styled from "styled-components";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <div>
+      <h4>comfy sloth starter</h4>
+    </div>
+  );
 }
 
-export default App
+export default App;
