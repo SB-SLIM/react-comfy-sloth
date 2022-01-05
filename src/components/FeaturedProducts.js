d10aad9b1426cd01efdfcdfe7be8c7b54e95a0b8
B <sbslim.dev@gmail.com>
2022-01-05 14:19:02 +0100

    RCS-16 hotfixe: slice map

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -23,11 +23,11 @@ const FeaturedProducts = () => {
   return (
     <Wrapper className="section">
       <div className="title">
-        feautred products
+        <h2>feautred products</h2>
         <div className="underline"></div>
       </div>
       <div className="section-center featured">
-        {featured.map((product) => {
+        {featured.slice(0, 3).map((product) => {
           return (
             <Product key={product.id} {...product}>
               {product.name}
